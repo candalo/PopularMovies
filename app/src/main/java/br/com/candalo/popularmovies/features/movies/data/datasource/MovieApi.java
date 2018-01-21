91e43e5a857ac26ef5eeac3c85e3ec3a357254c0
Lucas Candalo <candalolucas@gmail.com>
2018-01-21 12:17:10 -0200

    Add api endpoint to get movie reviews
    Signed-off-by: Lucas Candalo <candalolucas@gmail.com>

--- app/src/main/java/br/com/candalo/popularmovies/features/movies/data/datasource/MovieApi.java
+++ app/src/main/java/br/com/candalo/popularmovies/features/movies/data/datasource/MovieApi.java
@@ -3,6 +3,7 @@ package br.com.candalo.popularmovies.features.movies.data.datasource;
 
 import br.com.candalo.popularmovies.features.movies.domain.models.MovieResponseData;
 import br.com.candalo.popularmovies.features.movies.domain.models.MovieVideosResponseData;
+import br.com.candalo.popularmovies.features.movies.domain.models.MovieReviewResponseData;
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -17,4 +18,7 @@ public interface MovieApi {
 
     @GET("3/movie/{id}/videos")
     Observable<MovieVideosResponseData> getTrailers(@Path("id") int movieId, @Query("api_key") String apiKey);
+
+    @GET("3/movie/{id}/reviews")
+    Observable<MovieReviewResponseData> getReviews(@Path("id") int movieId, @Query("api_key") String apiKey);
 }
